78c8d19c07a0e978c273a29834fbf1c5934dafb8
unperknown <wnsdud105@gmail.com>
2020-04-13 21:45:28 +0900

    [8] Added : Upsert Operation

--- src/main/kotlin/space/devevents/DevEvents/resolvers/EventMutationResolver.kt
+++ src/main/kotlin/space/devevents/DevEvents/resolvers/EventMutationResolver.kt
@@ -3,15 +3,33 @@ package space.devevents.DevEvents.resolvers
 import graphql.kickstart.tools.GraphQLMutationResolver
 import space.devevents.DevEvents.entity.Event
 import space.devevents.DevEvents.repository.EventRepository
+import org.springframework.data.mongodb.core.MongoOperations
 import org.springframework.stereotype.Component
 import java.util.*
 
 @Component
-class EventMutationResolver(val eventRepository: EventRepository) : GraphQLMutationResolver {
+class EventMutationResolver(val eventRepository: EventRepository, private val mongoOperations: MongoOperations) : GraphQLMutationResolver {
     fun addEvent(event: Event): Event {
         eventRepository.save(event)
 
         return event
+    }
+
+    fun updateEvent(id: String, event: Event): Event {
+        val _event = eventRepository.findById(id)
+        _event.ifPresent {
+            it.title = event.title
+            it.location = event.location
+            it.date = event.date
+            it.price = event.price
+            it.hyperLink = event.hyperLink
+            it.imageLink = event.imageLink
+            it.isValid = event.isValid
+
+            eventRepository.save(it)
+        }
+
+        return _event.get()
     }
 
     fun removeEvent(id: String): Optional<Event> {
